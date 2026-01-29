04cbf588c7b5a3227735cf44dd05835882057c8f
Lasagnezio0 <gaviscon2007@gmail.com>
2026-01-29 18:21:58 +0100

    classe di cambio turno

---
+++ src/main/java/com/mycompany/battaglia_navale/payloads/TurnChangePayload.java
@@ -0,0 +1,5 @@
+package com.mycompany.battaglia_navale.payloads;
+public class TurnChangePayload {
+    private boolean yourTurn;
+    public TurnChangePayload(boolean yourTurn) { this.yourTurn = yourTurn; }
+}
